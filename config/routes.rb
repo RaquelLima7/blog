7dc97b8b74b0847739a3468299bece43b581a855
RaquelLima7 <raquellimadev@gmail.com>
2021-09-02 11:43:18 -0300

    resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/site', to: 'site#index'
   root to: 'site#index'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
