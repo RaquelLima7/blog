e3ba9818251abd3fbc5f058494b4477e8936cd40
RaquelLima7 <raquellimadev@gmail.com>
2021-09-03 11:44:31 -0300

    after_validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,11 @@
 class Author < ApplicationRecord
   validates :first_name, presence: true,  length: { minimum: 2, maximum: 100 }
-  validates :last_name, presence: true
   validates :description, allow_nil: true, length: { maximum: 500 }
+
+  after_validation :titleize_last_name, if: Proc.new { |a| a.last_name.present? }, on: :create
+
+  private
+  def titleize_last_name
+    self.last_name = last_name.titleize
+  end
 end
