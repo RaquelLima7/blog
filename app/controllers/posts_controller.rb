b1a937704209580b08ba7b25f5ef513f07f943a7
RaquelLima7 <raquellimadev@gmail.com>
2021-09-10 18:57:27 -0300

    create and update in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to '/posts'
+    else
+      render :new
+    end
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to '/posts'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -29,5 +40,9 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :publish_at, :author_id)
   end
 end
