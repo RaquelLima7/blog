089807c90eb5dce369d645265c126b0a214bd083
RaquelLima7 <raquellimadev@gmail.com>
2021-09-26 16:25:17 -0300

    new job

--- app/jobs/new_posts_notification_job.rb
+++ app/jobs/new_posts_notification_job.rb
@@ -2,6 +2,10 @@ class NewPostsNotificationJob < ApplicationJob
   queue_as :notification
 
   def perform(*args)
-    # Do something later
+    posts = Post.where(publish_at: (Time.now - 1.week)..).select(:title)
+    subscribers.each do |subscriber|
+      SubscribersMailer.with(subscriber: subscriber, posts: posts)
+        .new_posts.deliver_now
+    end
   end
 end
