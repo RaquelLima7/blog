a7aa52be49aa653bd887ef05c4f69f2fb9a7b92a
RaquelLima7 <raquellimadev@gmail.com>
2021-10-01 21:17:11 -0300

    add photos

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
   has_one_attached :avatar
+  has_many_attached :photos
   validates :first_name, presence: true,  length: { minimum: 2, maximum: 100 }
   validates :description, allow_nil: true, length: { maximum: 500 }
 
